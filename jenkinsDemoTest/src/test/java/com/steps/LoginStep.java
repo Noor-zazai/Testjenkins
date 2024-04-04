c061cea5f0570ff4c64d809b356d6906f1283c81
Noor-zazai <155582601+Noor-zazai@users.noreply.github.com>
2024-04-03 23:29:09 -0400

    Update LoginStep.java

--- jenkinsDemoTest/src/test/java/com/steps/LoginStep.java
+++ jenkinsDemoTest/src/test/java/com/steps/LoginStep.java
@@ -3,9 +3,7 @@ package com.steps;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.edge.EdgeDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
